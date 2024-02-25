86777dc37543401496b6aec1edc37297eb57e63e
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-02-25 18:40:08 +0100

    Add terraform script to generate worker vm

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -21,4 +21,13 @@ provider "proxmox" {
   # (Optional) Skip TLS Verification
   # pm_tls_insecure = true
 
+  ## For logging
+  pm_log_enable = true
+  pm_log_file   = "terraform-plugin-proxmox.log"
+  pm_debug      = true
+  pm_log_levels = {
+    _default    = "debug"
+    _capturelog = ""
+  }
+
 }

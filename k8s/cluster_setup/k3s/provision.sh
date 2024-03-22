4fad83d9170095475213e27156cf29688d13fabf
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-03-22 20:09:19 +0100

    feat: add calico support to k3s

--- k8s/cluster_setup/k3s/provision.sh
+++ k8s/cluster_setup/k3s/provision.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-export K3S_VERSION="v1.29.3-rc1+k3s1"
-#terraform -chdir=./infra init
+#export K3S_VERSION="v1.29.3-rc1+k3s1"
+export K3S_VERSION="v1.28.7+k3s1"
+
+## set the following for enabling calico cni instead of ootb flannel
+export INSTALL_CALICO=true
+## install calico and also disable traefik(ingress-controller)
+if [ "${INSTALL_CALICO}" = true ]; then
+    export K3S_CP_ADDITIONAL_CONFIG="--write-kubeconfig-mode 644 --flannel-backend=none --disable-network-policy --disable=traefik"
+fi
+
 terraform -chdir=./vm_provisioning apply --auto-approve &&
 
 ansible-playbook -i ./k8s_configuration/inventories/proxmox-inventory.proxmox.yml ./k8s_configuration/main-playbook.yml

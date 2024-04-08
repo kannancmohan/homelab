2921f61b3dbdd8717cfd4434489e98ffa2ed9028
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-04-08 10:16:42 +0200

    fix: update provisiong script

--- k8s/cluster_setup/k3s/provision.sh
+++ k8s/cluster_setup/k3s/provision.sh
@@ -2,23 +2,22 @@
 
 #export K3S_VERSION="v1.29.3-rc1+k3s1"
 export K3S_VERSION="v1.28.7+k3s1"
-
-## set the following to either ngnix or traefik
-export INGRESS_CONTROLLER="traefik"
 ## set the following to either flannel or calico
 export CNI_PLUGIN="flannel"
+## set the following to either ngnix or traefik
+export INGRESS_CONTROLLER="ngnix"
 
 ADDITIONAL_CONFIG="--write-kubeconfig-mode 644"
 
+## if cni-plugin is calico then disable ootb flannel
 if [ "${CNI_PLUGIN}" = "calico" ]; then
     ADDITIONAL_CONFIG="${ADDITIONAL_CONFIG} --flannel-backend=none --disable-network-policy"
 fi
 
-## install calico and also disable traefik(ingress-controller)
+## if ingress is ngnix then disable ootb traefik
 if [ "${INGRESS_CONTROLLER}" = "ngnix" ]; then
     ADDITIONAL_CONFIG="${ADDITIONAL_CONFIG} --disable=traefik"
 fi
-
 
 export K3S_CP_ADDITIONAL_CONFIG="$ADDITIONAL_CONFIG"
 

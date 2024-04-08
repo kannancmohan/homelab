185e2bc50135000b0096e1e13592aa93641c780e
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-04-08 10:07:14 +0200

    fix: update provisiong script

--- k8s/cluster_setup/k3s/provision.sh
+++ k8s/cluster_setup/k3s/provision.sh
@@ -3,12 +3,24 @@
 #export K3S_VERSION="v1.29.3-rc1+k3s1"
 export K3S_VERSION="v1.28.7+k3s1"
 
-## set the following for enabling calico cni instead of ootb flannel
-export INSTALL_CALICO=false
-## install calico and also disable traefik(ingress-controller)
-if [ "${INSTALL_CALICO}" = true ]; then
-    export K3S_CP_ADDITIONAL_CONFIG="--write-kubeconfig-mode 644 --flannel-backend=none --disable-network-policy --disable=traefik"
+## set the following to either ngnix or traefik
+export INGRESS_CONTROLLER="traefik"
+## set the following to either flannel or calico
+export CNI_PLUGIN="flannel"
+
+ADDITIONAL_CONFIG="--write-kubeconfig-mode 644"
+
+if [ "${CNI_PLUGIN}" = "calico" ]; then
+    ADDITIONAL_CONFIG="${ADDITIONAL_CONFIG} --flannel-backend=none --disable-network-policy"
 fi
+
+## install calico and also disable traefik(ingress-controller)
+if [ "${INGRESS_CONTROLLER}" = "ngnix" ]; then
+    ADDITIONAL_CONFIG="${ADDITIONAL_CONFIG} --disable=traefik"
+fi
+
+
+export K3S_CP_ADDITIONAL_CONFIG="$ADDITIONAL_CONFIG"
 
 terraform -chdir=./vm_provisioning apply --auto-approve &&
 

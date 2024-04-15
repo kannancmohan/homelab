864aba234b149c0dbd370550476cf7cdd1c2ec3d
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-04-15 18:51:31 +0200

    chor: update scripts folder

--- k8s/cluster_setup/k3s/scripts/provision.sh
+++ k8s/cluster_setup/k3s/scripts/provision.sh
@@ -1,27 +1,27 @@
-#!/bin/bash
+# #!/bin/bash
 
-#export K3S_VERSION="v1.29.3-rc1+k3s1"
-export K3S_VERSION="v1.28.7+k3s1"
-## set the following to either flannel or calico
-export CNI_PLUGIN="flannel"
-## set the following to either nginx or traefik
-export INGRESS_CONTROLLER="traefik"
-DISABLE_K3S_OOTB_TRAEFIK=true
+# #export K3S_VERSION="v1.29.3-rc1+k3s1"
+# export K3S_VERSION="v1.28.7+k3s1"
+# ## set the following to either flannel or calico
+# export CNI_PLUGIN="flannel"
+# ## set the following to either nginx or traefik
+# export INGRESS_CONTROLLER="traefik"
+# DISABLE_K3S_OOTB_TRAEFIK=true
 
-ADDITIONAL_CONFIG="--write-kubeconfig-mode 644"
+# ADDITIONAL_CONFIG="--write-kubeconfig-mode 644"
 
-## if cni-plugin is calico then disable ootb flannel
-if [ "${CNI_PLUGIN}" = "calico" ]; then
-    ADDITIONAL_CONFIG="${ADDITIONAL_CONFIG} --flannel-backend=none --disable-network-policy"
-fi
+# ## if cni-plugin is calico then disable ootb flannel
+# if [ "${CNI_PLUGIN}" = "calico" ]; then
+#     ADDITIONAL_CONFIG="${ADDITIONAL_CONFIG} --flannel-backend=none --disable-network-policy"
+# fi
 
-## if ingress is nginx then disable ootb traefik
-if [ "${INGRESS_CONTROLLER}" = "nginx" ] || [ "$DISABLE_K3S_OOTB_TRAEFIK" == true ]; then
-    ADDITIONAL_CONFIG="${ADDITIONAL_CONFIG} --disable=traefik"
-fi
+# ## if ingress is nginx then disable ootb traefik
+# if [ "${INGRESS_CONTROLLER}" = "nginx" ] || [ "$DISABLE_K3S_OOTB_TRAEFIK" == true ]; then
+#     ADDITIONAL_CONFIG="${ADDITIONAL_CONFIG} --disable=traefik"
+# fi
 
-export K3S_CP_ADDITIONAL_CONFIG="$ADDITIONAL_CONFIG"
+# export K3S_CP_ADDITIONAL_CONFIG="$ADDITIONAL_CONFIG"
 
-terraform -chdir=../vm_provisioning apply --auto-approve &&
+# terraform -chdir=../vm_provisioning apply --auto-approve &&
 
-ansible-playbook -i ../k8s_configuration/inventories/proxmox-inventory.proxmox.yml ../k8s_configuration/main-playbook.yml
+# ansible-playbook -i ../k8s_configuration/inventories/proxmox-inventory.proxmox.yml ../k8s_configuration/main-playbook.yml
--- scripts/k3s_install.sh
+++ scripts/k3s_install.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-sh ../k8s/cluster_setup/k3s/scripts/provision.sh
+#export K3S_VERSION="v1.29.3-rc1+k3s1"
+export K3S_VERSION="v1.28.7+k3s1"
+## set the following to either flannel or calico
+export CNI_PLUGIN="flannel"
+## set the following to either nginx or traefik
+export INGRESS_CONTROLLER="traefik"
+DISABLE_K3S_OOTB_TRAEFIK=true
+
+ADDITIONAL_CONFIG="--write-kubeconfig-mode 644"
+
+## if cni-plugin is calico then disable ootb flannel
+if [ "${CNI_PLUGIN}" = "calico" ]; then
+    ADDITIONAL_CONFIG="${ADDITIONAL_CONFIG} --flannel-backend=none --disable-network-policy"
+fi
+
+## if ingress is nginx then disable ootb traefik
+if [ "${INGRESS_CONTROLLER}" = "nginx" ] || [ "$DISABLE_K3S_OOTB_TRAEFIK" == true ]; then
+    ADDITIONAL_CONFIG="${ADDITIONAL_CONFIG} --disable=traefik"
+fi
+
+export K3S_CP_ADDITIONAL_CONFIG="$ADDITIONAL_CONFIG"
+
+terraform -chdir=../k8s/cluster_setup/k3s/vm_provisioning apply --auto-approve &&
+
+ansible-playbook -i ../k8s/cluster_setup/k3s/k8s_configuration/inventories/proxmox-inventory.proxmox.yml ../k8s/cluster_setup/k3s/k8s_configuration/main-playbook.yml

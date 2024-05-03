a752ddb8c9d44e9cfda17e97d0cb5c9d0cc8e67b
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-05-03 13:33:27 +0200

    feat: add passbolt  vm provisioning

--- proxmox/passbolt/vm_provisioning/main.tf
+++ proxmox/passbolt/vm_provisioning/main.tf
@@ -33,17 +33,6 @@ module "worker" {
   vm_tags                       = var.vm_tags
 }
 
-
-# output "uploaded_image_file_name" {
-#   value = proxmox_virtual_environment_download_file.ubuntu-qcow2-img.file_name
-# }
-# output "ubuntu_cloud_init_user_conf_file_name" {
-#   value = proxmox_virtual_environment_file.ubuntu-cloud-init-user-config.file_name
-# }
-# output "ubuntu_cloud_init_vendor_conf_file_name" {
-#   value = proxmox_virtual_environment_file.ubuntu-cloud-init-vendor-config.file_name
-# }
-
-# output "worker_vm_info" {
-#   value = module.worker
-# }
+output "worker_vm_info" {
+  value = module.worker
+}

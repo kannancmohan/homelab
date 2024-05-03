d2fbe44ba2a5a5295af3fdb459b3e9aeff6ee9cf
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-05-03 14:35:34 +0200

    feat: add passbolt  vm provisioning

---
+++ proxmox/passbolt/passbolt_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+terraform -chdir=./vm_provisioning apply --auto-approve &&
+
+ansible-playbook -i ./configuration/inventories/proxmox-inventory.proxmox.yml ./configuration/main-playbook.yml

dcc28b94080e2ddb9e508147a86addbe94ecd57c
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-05-05 15:22:36 +0200

    feat: add portainer vm provisioning

--- proxmox/portainer/portainer_install.sh
+++ proxmox/portainer/portainer_install.sh
@@ -2,4 +2,4 @@
 
 terraform -chdir=./vm_provisioning apply --auto-approve &&
 
-# ansible-playbook -i ./configuration/inventories/proxmox-inventory.proxmox.yml ./configuration/main-playbook.yml
+ansible-playbook -i ./configuration/inventories/proxmox-inventory.proxmox.yml ./configuration/main-playbook.yml

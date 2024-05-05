bd7dd3a78a1dc88162cc00e38798aa3681edf5a8
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-05-05 19:50:35 +0200

    chor: update adguard to use new terraform module

--- proxmox/adguard/vm_provisioning/main.tf
+++ proxmox/adguard/vm_provisioning/main.tf
@@ -1,87 +1,38 @@
-resource "proxmox_virtual_environment_download_file" "ubuntu-qcow2-img" {
-  file_name    = "adguard-ubuntu-cloudimg-amd64.img"
-  content_type = "iso"
-  datastore_id = var.proxmox_node_iso_datastore_id #the proxmox datastore if where iso are stored
-  node_name    = var.proxmox_node_name
-  url          = var.cloud_image_iso_url
-  # checksum           = var.cloud_image_iso_checksum
-  # checksum_algorithm = "sha256"
-  overwrite = true # if true and size of uploaded file is different than size from url Content-Length header, file will be downloaded again
-  #overwrite_unmanaged = true #if file with the same name already exists in the datastore, it will be deleted and the new file will be downloaded. If false and the file already exists, an error will be returned
-}
-
-resource "proxmox_virtual_environment_file" "ubuntu-cloud-init-user-config" {
-  content_type = "snippets"
-  datastore_id = var.proxmox_node_iso_datastore_id
-  node_name    = var.proxmox_node_name
-
-  source_raw {
-    data = <<EOF
-#cloud-config
-chpasswd:
-  list: |
-    ${var.vm_default_user_name}:${var.vm_default_user_pwd}
-  expire: false
-hostname: ${var.worker_vm_name}
-preserve_hostname: true
-timezone: ${var.vm_timezone}
-write_files:
-  - path: /etc/environment
-    content: |
-      SOME_ENV_VAR="test"
-    append: true
-users:
-  - default
-  - name: ${var.vm_default_user_name}
-    groups: sudo
-    shell: /bin/bash
-    ssh-authorized-keys:
-      - ${trimspace("${var.local_machine_ssh_key}")}
-    sudo: ALL=(ALL) NOPASSWD:ALL
-    EOF
-
-    file_name = var.ubuntu_cloud_init_user_config_file
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.47.0"
+    }
   }
 }
 
-resource "proxmox_virtual_environment_file" "ubuntu-cloud-init-vendor-config" {
-  content_type = "snippets"
-  datastore_id = var.proxmox_node_iso_datastore_id
-  node_name    = var.proxmox_node_name
-
-  source_raw {
-    data = <<EOF
-#cloud-config
-runcmd:
-    - apt update
-    - apt install -y qemu-guest-agent
-    - systemctl enable qemu-guest-agent
-    - systemctl start qemu-guest-agent
-    - echo "done" > /tmp/vendor-cloud-init-done
-    EOF
-
-    file_name = var.ubuntu_cloud_init_vendor_config_file
+provider "proxmox" {
+  endpoint = var.proxmox_api_endpoint
+  insecure = true
+  ssh {
+    agent    = true
+    username = "root" # can be set via environment variable PROXMOX_VE_SSH_USERNAME
   }
 }
 
 module "worker" {
-  source                           = "../../vm_provisioning/terraform_module"
-  count                            = var.worker_vm_count
-  vm_id                            = "${var.worker_vm_id_prefix}${count.index + 1}"
-  vm_name                          = "${var.worker_vm_name}-${count.index + 1}"
-  vm_node                          = var.proxmox_node_name
-  vm_description                   = "Adguard Home provisioned by terraform"
-  vm_cidr                          = "${var.worker_vm_ip_prefix}${count.index + 1}/24"
-  vm_gateway_ip                    = var.worker_vm_gateway_ip
-  vm_cpu_cores                     = var.worker_vm_cores
-  vm_memory                        = var.worker_vm_memory
-  vm_disk_file_id                  = proxmox_virtual_environment_download_file.ubuntu-qcow2-img.id
-  vm_disk_size                     = var.worker_vm_disk_size
-  vm_disk_ssd_enabled              = true
-  vm_tags                          = ["iac-tf", "os-ubuntu", "service-type-dns", "service-name-adguard"]
-  vm_agent_enabled                 = true
-  vm_cloudinit_user_data_file_id   = proxmox_virtual_environment_file.ubuntu-cloud-init-user-config.id
-  vm_cloudinit_vendor_data_file_id = proxmox_virtual_environment_file.ubuntu-cloud-init-vendor-config.id
-  vm_startup_order                 = "1"
-  vm_reboot                        = true
+  source                        = "../../vm_provisioning/terraform_modules/proxmox_single_vm"
+  proxmox_node_name             = var.proxmox_node_name
+  proxmox_node_iso_datastore_id = var.proxmox_node_iso_datastore_id
+  local_machine_ssh_key         = var.local_machine_ssh_key
+  vm_default_user_name          = var.vm_default_user_name
+  vm_default_user_pwd           = var.vm_default_user_pwd
+  vm_id                         = var.vm_id
+  vm_name                       = var.vm_name
+  vm_ip_addr                    = var.vm_ip_addr
+  vm_gateway_ip                 = var.vm_gateway_ip
+  vm_cpu_cores                  = var.vm_cores
+  vm_memory                     = var.vm_memory
+  vm_disk_size                  = var.vm_disk_size
+  vm_tags                       = var.vm_tags
+}
+
+output "worker_vm_info" {
+  value = module.worker
 }
--- proxmox/adguard/vm_provisioning/variables.tf
+++ proxmox/adguard/vm_provisioning/variables.tf
@@ -1,8 +1,4 @@
 # node variables
-variable "proxmox_api_endpoint" {
-  type        = string
-  description = "set the value for this variable using environment variable TF_VAR_proxmox_api_endpoint"
-}
 variable "proxmox_node_name" {
   type = string
 }
@@ -14,11 +10,9 @@ variable "proxmox_node_default_datastore_id" {
 }
 
 # project variables
-variable "cloud_image_iso_url" {
-  type = string
-}
-variable "cloud_image_iso_checksum" {
-  type = string
+variable "proxmox_api_endpoint" {
+  type        = string
+  description = "set the value for this variable using environment variable TF_VAR_proxmox_api_endpoint"
 }
 variable "local_machine_ssh_key" {
   type        = string
@@ -32,47 +26,30 @@ variable "vm_default_user_pwd" {
   type        = string
   description = "The default use password in vm. set the value using environment variable TF_VAR_vm_default_user_pwd"
 }
-variable "vm_timezone" {
-  type        = string
-  default     = "Europe/Berlin"
-  description = "The timezone for the vm"
-}
 
-
-# worker vm variables
-variable "worker_vm_name" {
+# vm variables
+variable "vm_name" {
   type = string
 }
-variable "worker_vm_id_prefix" {
+variable "vm_id" {
   type = number
 }
-variable "worker_vm_ip_prefix" {
+variable "vm_ip_addr" {
   type = string
 }
-variable "worker_vm_gateway_ip" {
+variable "vm_gateway_ip" {
   type = string
 }
-variable "worker_vm_count" {
-  type    = number
-  default = 1
-}
-variable "worker_vm_cores" {
+variable "vm_cores" {
   type = number
 }
-variable "worker_vm_memory" {
+variable "vm_memory" {
   type = number
 }
-variable "worker_vm_disk_size" {
+variable "vm_disk_size" {
   type        = number
   description = "Disk size in GB"
 }
-
-# proxmox cloud-init 
-variable "ubuntu_cloud_init_user_config_file" {
-  type    = string
-  default = "adguard-ubuntu-cloud-init-user-config.yaml"
-}
-variable "ubuntu_cloud_init_vendor_config_file" {
-  type    = string
-  default = "adguard-ubuntu-cloud-init-vendor-config.yaml"
+variable "vm_tags" {
+  type = list(any)
 }

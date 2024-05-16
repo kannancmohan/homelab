4d7f8ff8b1f6d87193fcff7d71a903f1f59b3e5e
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-05-16 15:26:11 +0200

    chor: modify terraform modules

--- proxmox/vaultwarden/install_vaultwarden.sh
+++ proxmox/vaultwarden/install_vaultwarden.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-terraform -chdir=./container_provisioning apply --auto-approve &&
+terraform -chdir=./provisioning apply --auto-approve &&
 
 ansible-playbook -i ./configuration/inventories/proxmox-inventory.proxmox.yml ./configuration/main-playbook.yml

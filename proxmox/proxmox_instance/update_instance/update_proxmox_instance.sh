50e0ea6cc6c850a4c62bcf1714f8d96697c2a89c
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-05-19 14:33:00 +0200

    feat: add proxmox update script

--- proxmox/proxmox_instance/update_instance/update_proxmox_instance.sh
+++ proxmox/proxmox_instance/update_instance/update_proxmox_instance.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i $PROXMOX_VE_ENDPOINT_IP, main-playbook.yml
+ansible-playbook -i $PROXMOX_VE_ENDPOINT_IP, main-playbook.yml --extra-vars "ansible_user=${PROXMOX_VE_SSH_USER}"

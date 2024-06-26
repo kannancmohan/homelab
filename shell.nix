f4c32417362584da413248aadb6b2ec7a09d60fe
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-06-26 15:58:32 +0200

    fix: update shell.nix to use ssh and remote docker

--- shell.nix
+++ shell.nix
@@ -1,27 +1,65 @@
 let
     nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
     pkgs = import nixpkgs { config = {}; overlays = []; };
+    isMacOS = pkgs.stdenv.hostPlatform.system == "darwin";
+    useRemoteDocker = false;
+    pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+        molecule
+        docker
+        ansible
+        pytest-testinfra
+        # Add any additional Python packages you need
+    ]);
 in
 
 pkgs.mkShellNoCC {
     packages = with pkgs; [
+        pythonEnv
+        pkgs.docker
         pkgs.kubectl
         pkgs.kubernetes-helm
         pkgs.k9s
         pkgs.copier
+        ## added for ansible test
+        pkgs.ansible-lint
+        pkgs.yamllint
+        ## added for ssh access
+        pkgs.git
+        pkgs.curl
         #pkgs.argocd
         #pkgs.vaultwarden
         #pkgs.prometheus-alertmanager
         #pkgs.postgresql_jit
     ];
-    HISTCONTROL = "ignoreboth:erasedups";
 
+    HISTCONTROL = "ignoreboth:erasedups";
     #KUBECONFIG = "~/.kube/config";
 
     shellHook = ''
-    alias k="kubectl"
-    alias kg="kubectl get"
-    alias tf="terraform"
-    #helm plugin install https://github.com/databus23/helm-diff
+        alias k="kubectl"
+        alias kg="kubectl get"
+        alias tf="terraform"
+        #helm plugin install https://github.com/databus23/helm-diff
+        
+        if [[ "${toString useRemoteDocker}" == "true" ]]; then
+            echo "Using remote Docker and setting ssh for the same";
+            export DOCKER_HOST=ssh://ubuntu@192.168.0.30
+            ## Start SSH agent if not already running
+            if [ -z "$SSH_AGENT_PID" ]; then
+                eval $(ssh-agent -s)
+                echo "SSH agent started with PID $SSH_AGENT_PID"
+            fi
+            ## Add host ssh keys
+            if $isMacOS; then
+                ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+            else
+                ssh-add ~/.ssh/id_ed25519
+            fi
+            ## Export the SSH_AUTH_SOCK so that it's available in the shell
+            export SSH_AUTH_SOCK=$SSH_AUTH_SOCK
+        else
+            echo "Using local Docker";
+        fi
+        
     '';
 }

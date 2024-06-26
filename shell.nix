68d9b9437179991a1513ad6396c2ad5151c2ba52
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-06-26 11:03:46 +0200

    fix: format nix shell

--- shell.nix
+++ shell.nix
@@ -1,27 +1,27 @@
 let
-nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
-pkgs = import nixpkgs { config = {}; overlays = []; };
+    nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+    pkgs = import nixpkgs { config = {}; overlays = []; };
 in
 
 pkgs.mkShellNoCC {
-packages = with pkgs; [
-    pkgs.kubectl
-    pkgs.kubernetes-helm
-    pkgs.k9s
-    pkgs.copier
-    #pkgs.argocd
-    #pkgs.vaultwarden
-    #pkgs.prometheus-alertmanager
-    #pkgs.postgresql_jit
-];
-HISTCONTROL = "ignoreboth:erasedups";
+    packages = with pkgs; [
+        pkgs.kubectl
+        pkgs.kubernetes-helm
+        pkgs.k9s
+        pkgs.copier
+        #pkgs.argocd
+        #pkgs.vaultwarden
+        #pkgs.prometheus-alertmanager
+        #pkgs.postgresql_jit
+    ];
+    HISTCONTROL = "ignoreboth:erasedups";
 
-#KUBECONFIG = "~/.kube/config";
+    #KUBECONFIG = "~/.kube/config";
 
-shellHook = ''
-alias k="kubectl"
-alias kg="kubectl get"
-alias tf="terraform"
-#helm plugin install https://github.com/databus23/helm-diff
-'';
+    shellHook = ''
+    alias k="kubectl"
+    alias kg="kubectl get"
+    alias tf="terraform"
+    #helm plugin install https://github.com/databus23/helm-diff
+    '';
 }

68f40e1e69a96390c0d99f7e8fd8565cfdc3a905
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-07-03 12:24:35 +0200

    feat: add molecule test to vaultwarden

---
+++ proxmox/vaultwarden/molecule/default/tests/test_default.py
@@ -0,0 +1,15 @@
+import testinfra
+
+def test_service_running(host):
+    s = host.service('vaultwarden')
+    assert s.is_running
+    assert s.is_enabled
+    
+def test_web_url_accessible(host):
+    output = host.run("curl http://localhost:8000")
+    assert output.rc == 0
+    assert output.succeeded
+
+def test_port_listening(host):
+    socket = host.socket("tcp://0.0.0.0:800")
+    assert socket.is_listening
